89288bbda283c2c0ea14bc70228621f6e2fa6720
unknown <brunonavarrodavi@gmail.com>
2025-05-27 10:55:48 -0300

    Exercicío 3

--- Ex3.js
+++ Ex3.js
@@ -7,12 +7,12 @@ for (let i = 0; i < 10; i++) {// loop de repetição que pede 10 números inteir
 
 let numeroParaBuscar = parseInt(prompt("Digite um número para verificar quantas vezes ele aparece:")); //cria a variavel de busca e ja pede para o usuario informar o valor que ele quer procurar
 
-let contadorU = 0; //cria a variavel contadora (e nao é o i) e inicializa ela com 0
+let vezes = 0; //cria a variavel contadora (e nao é o i) e inicializa ela com 0
 
 for (let i = 0; i < 10; i++) { //loop de repeticão que verifica numero por numero e armazena a quantidade de vezes que ele apareceu e armazena na variavel (contadorU)
     if (numeros[i] === numeroParaBuscar) {
-        contadorU++;
+        vezes++;
     }
 }
 
-alert(`O número ${numeroParaBuscar} aparece ${contadorU} vezes no vetor.`);
+alert(`O número ${numeroParaBuscar} aparece ${vezes} vezes no vetor.`);
