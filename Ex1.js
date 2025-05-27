ca1b43e042dec5c6d77a54339160aeea1487a37c
unknown <brunonavarrodavi@gmail.com>
2025-05-27 10:08:19 -0300

    Exercicío 1

--- Ex1.js
+++ Ex1.js
@@ -1,15 +1,15 @@
-let numeros = [];
+let numeros = []; // cria o vetor numeros 
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) { // loop de repetiçao que pede os 10 numeros ao usuario
     let num = parseInt(prompt(`Digite o ${i + 1}º número inteiro:`));
-    numeros[i] = num;
+    numeros[i] = num; // iguala[i] números a num
 }
 
-let somaDosPares = 0;
+let somaDosPares = 0; // cria variavel da soma e inicializa ela com zero
 
-for (let i = 0; i < 10; i++) {
-    if (numeros[i] % 2 === 0) {
-        somaDosPares += numeros[i];
+for (let i = 0; i < 10; i++) { // loop de repetição que verifica os numeros pares e faz soma
+    if (numeros[i] % 2 === 0) { // condição de verificação que pega o resto de divisão se for real ele soma os numeros pares
+        somaDosPares += numeros[i]; // soma dos numeros pares que veem do numeros
     }
 }
 
