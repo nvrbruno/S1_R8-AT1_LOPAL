9bd3170117f33d2b6295c5978fb8d3bae89c1e09
unknown <brunonavarrodavi@gmail.com>
2025-05-27 08:47:30 -0300

    Exercicío 1

--- Ex1.js
+++ Ex1.js
@@ -1,15 +1,15 @@
 let numeros = [];
 
 for (let i = 0; i < 10; i++) {
-    let numero = parseInt(prompt(`Digite o ${i + 1}º número inteiro:`));
-    numeros[i] = numero;
+    let num = parseInt(prompt(`Digite o ${i + 1}º número inteiro:`));
+    numeros[i] = num;
 }
 
-let somaPares = 0;
+let somaDosPares = 0;
 
 for (let i = 0; i < 10; i++) {
     if (numeros[i] % 2 === 0) {
-        somaPares += numeros[i];
+        somaDosPares += numeros[i];
     }
 }
 
