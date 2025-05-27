878da596ed05c90e711b9b2d907260e7f2da07b9
unknown <brunonavarrodavi@gmail.com>
2025-05-27 07:45:28 -0300

    Exercicío 1

---
+++ Ex1.js
@@ -0,0 +1,16 @@
+let numeros = [];
+
+for (let i = 0; i < 10; i++) {
+    let numero = parseInt(prompt(`Digite o ${i + 1}º número inteiro:`));
+    numeros[i] = numero;
+}
+
+let somaPares = 0;
+
+for (let i = 0; i < 10; i++) {
+    if (numeros[i] % 2 === 0) {
+        somaPares += numeros[i];
+    }
+}
+
+alert("A soma dos números pares é: " + somaPares);
